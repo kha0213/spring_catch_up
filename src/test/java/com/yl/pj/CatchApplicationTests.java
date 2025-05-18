85017336f517448920214908f2f204f00ee298ac
moon maria <tatujjang@gmail.com>
2025-05-18 23:49:04 +0900

    코드 재정비 Spring 3.4.5 java 21로 변경

--- src/test/java/com/yl/pj/CatchApplicationTests.java
+++ src/test/java/com/yl/pj/CatchApplicationTests.java
@@ -3,15 +3,12 @@ package com.yl.pj;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Stack;
-import java.util.Vector;
-
 @SpringBootTest
 class CatchApplicationTests {
 
     @Test
     void contextLoads() {
-        Vector
+
     }
 
 }
